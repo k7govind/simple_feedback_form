e8c3b296a47d232f6358a6aca79ee8777b7f7060
Govindarajan K <govindra@michaels.com>
2025-01-11 22:06:48 +0530

    Added validate form script

--- middlewares/validateForm.mjs
+++ middlewares/validateForm.mjs
@@ -1,16 +1,16 @@
-import validator, { isEmpty, isAlpha } from 'validator';
+import validator from 'validator';
 export const validateForm = (req,res,next)=>{
     const formdata = req.body;
     var noEmptyFields = false;
     var validatedEmail = false;
     var ensureString = false;
-    if(formdata != null && !isEmpty(formdata)) {
+    if(formdata != null && !validator.isEmpty(formdata)) {
         const {firstname, lastname, useremail, userfeedback} = formdata;
-        if(!isEmpty(firstname) && !isEmpty(lastname) && !isEmpty(useremail)) {
+        if(!validator.isEmpty(firstname) && !validator.isEmpty(lastname) && !validator.isEmpty(useremail)) {
             noEmptyFields = true;
         }
         validatedEmail = validator.isEmail(useremail);
-        if(isAlpha(firstname) && isAlpha(lastname)) {
+        if(validator.isAlpha(firstname) && validator.isAlpha(lastname)) {
             ensureString = true;
         }
         return (noEmptyFields && validatedEmail && ensureString) ? true : false;
